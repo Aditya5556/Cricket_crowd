927a6f7a9c7a790276cee6118d35cb4f1af1828c
Khaja Moinuddin Khadri <80167150+KHAJAMOINUDDINKHADRI@users.noreply.github.com>
2025-06-11 17:47:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './components/LandingPage';
+import SubmitStory from './components/StorySubmit';
+import Stories from './components/Stories';
+import MonetizationModules from './components/MonetizationModules';
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/submit" element={<SubmitStory />} />
+          <Route path="/stories" element={<Stories />} />
+          <Route path="/monetization" element={<MonetizationModules />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
